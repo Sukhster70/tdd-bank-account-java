8c38137c73b87a6e960ce87a6e975baa223e8511
Sukhster70 <sukhbir_bassi@yahoo.co.uk>
2021-01-28 15:44:23 +0000

    Initial commit.

--- src/test/java/org/xpdojo/bank/AccountTest.java
+++ src/test/java/org/xpdojo/bank/AccountTest.java
@@ -2,14 +2,14 @@ package org.xpdojo.bank;
 
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+import org.xpdojo.bank.Account;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class AccountTest {
 
     @Test
-	@Disabled
-    public void depositAnAmountToIncreaseTheBalance() {
-        assertThat("your first test").isBlank();
+    public void depositAmount() {
+        Account account = new Account();
     }
 }
